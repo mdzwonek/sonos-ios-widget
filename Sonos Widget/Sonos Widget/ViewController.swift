480f98410fa475702af1aa6856fec46df6bc2b5a
Mateusz Dzwonek <mateusz.dzwonek@gmail.com>
2015-01-11 15:56:26 +0000

    Created project.

---
+++ Sonos Widget/Sonos Widget/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Sonos Widget
+//
+//  Created by Mateusz Dzwonek on 11/01/2015.
+//  Copyright (c) 2015 Mateusz Dzwonek. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
