8e2e2a6400075bbe90858041c513334b0f282815
Mateusz Dzwonek <mateusz.dzwonek@gmail.com>
2015-01-12 19:25:03 +0000

    Disable UI when Sonos is disconnected.

--- Sonos Widget/Widget Extension/TodayViewController.swift
+++ Sonos Widget/Widget Extension/TodayViewController.swift
@@ -11,20 +11,45 @@ import NotificationCenter
 
 class TodayViewController: UIViewController, NCWidgetProviding {
     
+    @IBOutlet var previousSongButton: UIButton?
+    @IBOutlet var nextSongButton: UIButton?
+    @IBOutlet var playPauseButton: UIButton?
+    @IBOutlet var volumeUpButton: UIButton?
+    @IBOutlet var volumeDownButton: UIButton?
+    @IBOutlet var volumeImageView: UIImageView?
+    
     var sonosManager: SonosManager?
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        self.preferredContentSize = CGSizeMake(CGRectGetWidth(self.view.frame), 75);
+        self.preferredContentSize = CGSizeMake(CGRectGetWidth(self.view.frame), 75)
     }
     
     func widgetPerformUpdateWithCompletionHandler(completionHandler: ((NCUpdateResult) -> Void)!) {
+        self.setEnabled(false)
+        self.sonosManager?.removeObserver(self, forKeyPath: "currentDevice")
         self.sonosManager = SonosManager()
+        self.sonosManager?.addObserver(self, forKeyPath: "currentDevice", options: .New, context: nil)
         completionHandler(NCUpdateResult.NewData)
     }
     
     func widgetMarginInsetsForProposedMarginInsets(defaultMarginInsets: UIEdgeInsets) -> UIEdgeInsets {
         return UIEdgeInsetsMake(0, 50, 0, 0)
+    }
+    
+    override func observeValueForKeyPath(keyPath: String, ofObject object: AnyObject, change: [NSObject : AnyObject], context: UnsafeMutablePointer<Void>) {
+        if keyPath == "currentDevice" {
+            dispatch_async(dispatch_get_main_queue(), { () -> Void in
+                self.setEnabled(true)
+            })
+        }
+    }
+    
+    func setEnabled(enabled: Bool) {
+        for button in [ self.previousSongButton, self.nextSongButton, self.playPauseButton, self.volumeUpButton, self.volumeDownButton ] {
+            button?.enabled = enabled
+        }
+        self.volumeImageView?.alpha = enabled ? 1.0 : 0.5
     }
     
     @IBAction func didTapPreviousButton() {
@@ -53,6 +78,10 @@ class TodayViewController: UIViewController, NCWidgetProviding {
                 self.sonosManager?.currentDevice?.setVolume(volume + volumeDelta, completion: nil)
             }
         })
+    }
+    
+    deinit {
+        self.sonosManager?.removeObserver(self, forKeyPath: "currentDevice")
     }
     
 }
